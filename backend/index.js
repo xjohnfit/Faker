4a5ed9cdfe79602f97a670ea3e698bbdd0f0f146
John Rocha <xjohnfitcodes@gmail.com>
2024-06-11 01:12:40 -0400

    generate function in api.js

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const app = express()
-const port = 3000
-
-app.get('/howitworks', (req, res) => res.send('Hello World!'));
-app.get('/'), (req, res) => { }
-app.listen(port, () => console.log(`App listening on port ${port}!`))
--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -1,70 +1,9 @@
+import React from 'react'
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { generate } from "../../backend/api.js";
 
-export default function Form() {
-
-  const [name, setName] = useState("Name will be populated here");
-  const [lastName, setLastName] = useState("Last name will be populated here");
-  const [email, setEmail] = useState("Email will be populated here");
-  const [gender, setGender] = useState("Gender will be populate here")
-  const [address, setAddress] = useState("Address will be populate here")
-  const [country, setCountry] = useState("Country will be populate here")
-  
-  const emails = [
-    {
-      "id": 1,
-      "domain": "icloud.com"
-    },
-    {
-      "id": 2,
-      "domain": "gmail.com"
-    },
-    {
-      "id": 3,
-      "domain": "yahoo.com"
-    },
-    {
-      "id": 4,
-      "domain": "hotmail.com"
-    }
-  ]
-
-  const generate = async (event) => {
-    event.preventDefault();
-
-    const options = {
-      method: 'POST',
-      url: 'https://fakerpro.p.rapidapi.com/schema',
-      params: {amount: '1'},
-      headers: {
-        'x-rapidapi-key': '46e4600040msh2fc1496a0ac3011p147823jsn2ca935ec8b45',
-        'x-rapidapi-host': 'fakerpro.p.rapidapi.com',
-        'Content-Type': 'application/json'
-      },
-      data: {
-        full_name: 'name',
-        person_age: 'age',
-        person_gender: 'gender',
-        person_address: 'address',
-        postal_code: 'postal',
-        country_code: 'country',
-      }
-    };
-    
-    await axios.request(options)
-    .then((response) => { 
-      console.log(response.data); 
-      setName(response.data.full_name.split(' ')[0]);
-      setLastName(response.data.full_name.split(' ')[1]);
-      setEmail(response.data.full_name.replace(' ', '.')+'@' + emails[Math.floor(Math.random() * emails.length)].domain); 
-      setGender(response.data.person_gender);
-      setAddress(response.data.person_address);
-      setCountry(response.data.country_code);
-    })
-    .catch((error) => { console.error(error); })
-  }
-
+const Form = () => {
   return (
     <form>
       <div className="space-y-12">
@@ -287,3 +226,5 @@ export default function Form() {
     </form>
   );
 }
+
+export default Form
